91546ff3e28ba59a5da61ecc6fb32a9761d7bcf7
Lê Việt Thắng <levietthang1997@gmail.com>
2021-05-07 23:49:12 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-app';
+  currentFeature = 'recipes';
+
+  updateCurentFeature($event: string) {
+    this.currentFeature = $event;
+  }
 }
